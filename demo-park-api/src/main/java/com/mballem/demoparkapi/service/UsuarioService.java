ed767dc12216f976d8a8e0fec8d19910a223754d
Mateus Santos <mateus_saantos@outlook.com>
2024-01-12 19:04:27 -0300

    Primeira parte aplicação

---
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
@@ -0,0 +1,19 @@
+package com.mballem.demoparkapi.service;
+
+import com.mballem.demoparkapi.entity.Usuario;
+import com.mballem.demoparkapi.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+}
