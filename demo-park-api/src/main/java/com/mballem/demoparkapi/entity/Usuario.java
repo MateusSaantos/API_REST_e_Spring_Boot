02c01d5006e6a1060a4320e002cde1a82ca468ad
Mateus Santos <mateus_saantos@outlook.com>
2024-01-16 15:00:30 -0300

    Segunda parte aplicação

--- demo-park-api/src/main/java/com/mballem/demoparkapi/entity/Usuario.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/entity/Usuario.java
@@ -1,29 +1,25 @@
 package com.mballem.demoparkapi.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
 public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Id")
-    private long id;
+    @Column(name = "id")
+    private Long id;
     @Column(name = "username", nullable = false, unique = true, length = 100)
     private String username;
-    @Column(name = "password", nullable = false, unique = false, length = 200)
+    @Column(name = "password", nullable = false, length = 200)
     private String password;
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false, length = 25)
@@ -39,7 +35,7 @@ public class Usuario implements Serializable {
     private String modificadoPor;
 
     public enum Role {
-
+        ROLE_ADMIN, ROLE_CLIENTE
     }
 
     @Override
@@ -47,7 +43,7 @@ public class Usuario implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Usuario usuario = (Usuario) o;
-        return id == usuario.id;
+        return Objects.equals(id, usuario.id);
     }
 
     @Override
